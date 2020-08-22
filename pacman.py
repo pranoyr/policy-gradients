08d3baa25f964a832fad0d4fca582f4551442646
Pranoy R <pranoyalkr@gmail.com>
2020-08-23 02:10:42 +0530

    initial commit

--- pacman.py
+++ pacman.py
@@ -23,18 +23,10 @@ parser.add_argument('--log-interval', type=int, default=10, metavar='N',
 args = parser.parse_args()
 
 
-# env = gym.make('SuperMarioBros-1-1-v0')
-from nes_py.wrappers import JoypadSpace
-import gym_super_mario_bros
-from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
-env = gym_super_mario_bros.make('SuperMarioBros-v0')
-env = JoypadSpace(env, SIMPLE_MOVEMENT)
-
+env = gym.make('MsPacman-v0')
 env.seed(args.seed)
 torch.manual_seed(args.seed)
 
-use_cuda = torch.cuda.is_available()
-device = torch.device(f"cuda:0" if use_cuda else "cpu")
 
 class Policy(nn.Module):
 	def __init__(self):
@@ -42,7 +34,6 @@ class Policy(nn.Module):
 		self.layer = resnet18(pretrained=False)
 		self.layer.fc = nn.Sequential(
 			nn.Linear(512, 5))
-			
 		self.saved_log_probs = []
 		self.rewards = []
 
@@ -82,25 +73,17 @@ class Policy(nn.Module):
 # 		return F.softmax(action_scores, dim=1)
 
 
-policy = Policy().to(device)
+policy = Policy()
 optimizer = optim.Adam(policy.parameters(), lr=1e-3)
 eps = np.finfo(np.float32).eps.item()
 
 
-checkpoint = torch.load('./models/model.pth', map_location='cpu')
-policy.load_state_dict(checkpoint['model_state_dict'])
-
-# policy.eval()
-
-
-
 def select_action(state):
-	state = state.copy()
-	state = torch.from_numpy(state).float().unsqueeze(0).permute(0, 3, 1, 2).to(device)
+	state = torch.from_numpy(state).float().unsqueeze(0).permute(0, 3, 1, 2)
 	probs = policy(state)
 	m = Categorical(probs)
 	action = m.sample()
-	# policy.saved_log_probs.append(m.log_prob(action))
+	policy.saved_log_probs.append(m.log_prob(action))
 	return action.item()
 
 
@@ -113,7 +96,7 @@ def finish_episode():
 		R = r + args.gamma * R
 		returns.insert(0, R)
 	returns = torch.tensor(returns)
-	returns = (returns - returns.mean()) / (returns.std() + eps)
+	# returns = (returns - returns.mean()) / (returns.std() + eps)
 	for log_prob, R in zip(policy.saved_log_probs, returns):
 		policy_loss.append(-log_prob * R)
 	optimizer.zero_grad()
@@ -128,25 +111,21 @@ def main():
 	running_reward = 10
 	for i_episode in count(1):
 		state, ep_reward = env.reset(), 0
-		for t in range(1, 1000000):  # Don't infinite loop while learning
+		for t in range(1, 200):  # Don't infinite loop while learning
 			action = select_action(state)
 			state, reward, done, _ = env.step(action)
 			# print(state)
 			# if args.render:
 			env.render()
-			# policy.rewards.append(reward)
+			policy.rewards.append(reward)
 			ep_reward += reward
-			print(ep_reward)
 			if done:
 				print(f"finised episode at iteration {t}")
 				break
 
 		running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
 		print(f"Running Reward : {running_reward}")
-		# finish_episode()
-
-		# state = {'model_state_dict': policy.state_dict()}
-		# torch.save(state, './models/model.pth')
+		finish_episode()
 
 		# if i_episode % args.log_interval == 0:
 		#     print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
