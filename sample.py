9244ef9163860e267e34c9345d7610b756a01902
Pranoy R <pranoyalkr@gmail.com>
2020-08-26 00:20:01 +0530

    initial commit

--- sample.py
+++ sample.py
@@ -1,16 +1,26 @@
 import torch
+from torch.distributions import Categorical
+
 
 batch_size = 2
 
-preds = torch.tensor([[7,2,3,4],
-					  [1,2,3,4]], dtype=torch.float32)
+while True:
+	preds = torch.tensor([[0.7,0.1,0.1,0.1]], dtype=torch.float)
 
-labels = torch.tensor([[1,0,0,0],
-					  [0,1,0,0]], dtype=torch.long)
+
+
+	m = Categorical(preds)
+	action = m.sample()
+	print(action)
+
+
+
+# labels = torch.tensor([[1,0,0,0],
+# 					  [0,1,0,0]], dtype=torch.long)
 				  
 
-l = -torch.mean(torch.sum(labels * torch.log(preds), dim=1))
-print(labels * torch.log(preds))
-l = -torch.mean(torch.sum(labels.view(batch_size, -1) * torch.log(preds.view(batch_size, -1)), dim=1))
+# l = -torch.mean(torch.sum(labels * torch.log(preds), dim=1))
+# print(labels * torch.log(preds))
+# l = -torch.mean(torch.sum(labels.view(batch_size, -1) * torch.log(preds.view(batch_size, -1)), dim=1))
 
-print(l)
+# print(l)
